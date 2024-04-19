b8326cc5d4c90b3435e532fa23b6dc4197f47b31
brian kanari <briangitui26@gmail.com>
2024-04-19 16:11:25 +0300

    adds index.js solution

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(
+      function (match) { return match.toLowerCase() === string.toLowerCase()
+    });
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(
+      function(Match) { return Match.toLowerCase().indexOf(letter.toLowerCase()) === 0
+    });
+}
+
+function matchName(drivers, Name) {
+    return drivers.filter(
+        function(record) { return record.name === Name});
+}
